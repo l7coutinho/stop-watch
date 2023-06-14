7ca5c85c20b9e65fdd2040ca84c163fae90e99fe
Lucas Coutinho <lucxscout@gmail.com>
2023-06-14 17:50:28 -0300

    fix: add variavel intervalo

--- main.js
+++ main.js
@@ -1,88 +1,78 @@
-const min = document.querySelector(".minutes");
-const sec = document.querySelector(".seconds");
-const milisec = document.querySelector(".milisseconds");
+const min = document.querySelector('.minutes');
+const sec = document.querySelector('.seconds');
+const milisec = document.querySelector('.milisseconds');
 
-const startTimer = document.querySelector(".start");
-const pauseTimer = document.querySelector(".pause");
-const resetTimer = document.querySelector(".reset");
+const startTimer = document.querySelector('.start');
+const pauseTimer = document.querySelector('.pause');
+const resetTimer = document.querySelector('.reset');
 
 //VÁRIAVEIS
-let miliNum = 0
-let secNum = 0
-let minNum = 0
-
-
+let miliNum = 0;
+let secNum = 0;
+let minNum = 0;
+let INTERVALO;
 
 function miliseconds() {
-    miliNum++
-    if (miliNum < 10) {
-        milisec.innerHTML = '0' + miliNum
-    } else {
-        milisec.innerHTML = miliNum
-    }
-    if (miliNum == 99) {
-        miliNum = 0
-        seconds()
-    }
+  miliNum++;
+  if (miliNum < 10) {
+    milisec.innerHTML = '0' + miliNum;
+  } else {
+    milisec.innerHTML = miliNum;
+  }
+  if (miliNum == 99) {
+    miliNum = 0;
+    seconds();
+  }
 }
 
-function seconds () {
-    secNum++
-    if (secNum < 10) {
-        sec.innerHTML = '0' + secNum
-    } else {
-        sec.innerHTML = secNum
-    }
-    if (secNum == 60) {
-        secNum = 0
-        minute()
-    }
-
+function seconds() {
+  secNum++;
+  if (secNum < 10) {
+    sec.innerHTML = '0' + secNum;
+  } else {
+    sec.innerHTML = secNum;
+  }
+  if (secNum == 60) {
+    secNum = 0;
+    minute();
+  }
 }
 
-function minute () {
-    minNum++
-    if (minNum < 10) {
-        min.innerHTML = '0' + minNum
-    } else {
-        min.innerHTML = minNum
-    }
+function minute() {
+  minNum++;
+  if (minNum < 10) {
+    min.innerHTML = '0' + minNum;
+  } else {
+    min.innerHTML = minNum;
+  }
 }
 
 //EVENTOS INICIAR PAUSAR E RESETAR
 
 startTimer.addEventListener('click', function (s) {
-    s.preventDefault;
+  s.preventDefault;
 
-    clearInterval(INTERVALO)
-    INTERVALO = setInterval(() => {
-        miliseconds()
-    }, 10)
-
-    
-})
+  clearInterval(INTERVALO);
+  INTERVALO = setInterval(() => {
+    miliseconds();
+  }, 10);
+});
 
 pauseTimer.addEventListener('click', function (p) {
-    p.preventDefault
+  p.preventDefault;
 
-    clearInterval(INTERVALO)
-})
+  clearInterval(INTERVALO);
+});
 
 resetTimer.addEventListener('click', function (r) {
-    r.preventDefault
-    clearInterval(INTERVALO)
+  r.preventDefault;
+  clearInterval(INTERVALO);
 
-    miliNum = 0
-    secNum = 0
-    minNum = 0
+  miliNum = 0;
+  secNum = 0;
+  minNum = 0;
 
-    milisec.innerHTML = '00'
-    sec.innerHTML = '00'
-    min.innerHTML = '00'
-})
-
-
-
-
-
-
+  milisec.innerHTML = '00';
+  sec.innerHTML = '00';
+  min.innerHTML = '00';
+});
